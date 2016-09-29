13ca92c12a2d18ad30fb8a1c80afe45728b69e5b
Linda Speight <linda@reachmaine.com>
2016-09-29 09:33:56 -0400

    change custom post type to 'obituary' instead of 'ea_obit'

--- origmag-ea/inc/eai_obits.php
+++ origmag-ea/inc/eai_obits.php
@@ -3,7 +3,7 @@
 add_action ('init', 'create_obit_posttype');
 if (!function_exists('create_obit_posttype')) {
 	function create_obit_posttype() {
-		 register_post_type( 'ea_obit',
+		/*  register_post_type( 'ea_obit',
 		    array (
 		      'labels' => array(
 		        'name' => __( 'Obits' ),
@@ -14,7 +14,21 @@ if (!function_exists('create_obit_posttype')) {
 		      'has_archive' => true,
 		      'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
 		      'rewrite' => array('slug' => 'obituary'),
+		)   ); */
+		register_post_type( 'obituary',
+		    array (
+		      'labels' => array(
+		        'name' => __( 'Obituarys' ),
+		        'singular_name' => __( 'Obituary' )
+		    ),
+		      'taxonomies' => array('category', 'post_tag'),  
+		      'public' => true,
+		      'has_archive' => true,
+		      'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
+		      'rewrite' => array('slug' => 'obituary'),
 		)   );
+		//flush_rewrite_rules();
 	}
 }
+
 ?>
