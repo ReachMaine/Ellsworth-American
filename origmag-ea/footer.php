61f34660081bb2f66ac20378b9e1db5ab9be973c
linda@reachmaine.com <linda@reachmaine.com>
2018-08-13 11:25:37 -0400

    setup new widget area for yellow pages.

--- origmag-ea/footer.php
+++ origmag-ea/footer.php
@@ -1,12 +1,20 @@
-<?php /* 
+<?php /*
 	modifications:
-		10Oct14 - zig - modify copyright to 
+		10Oct14 - zig - modify copyright to
 		24Dec14 - zig - add broadstreet popup script
 		31jan16 - zig - add technavia script at bottom of page
 		29Aug16 - zig - xout technavia script.  put into header.
 */ ?>
 <?php global $theme_url, $prl_data;?>
+
 	<footer id="footer">
+		<?php
+			if (is_active_sidebar('over_footer')) {
+				echo '<div id="over_footer_wrap" class="prl-container">';
+				dynamic_sidebar('over_footer');
+				echo '</div>';
+			}
+		?>
 		<?php if($prl_data['footer_widget']!='Disable'):?>
 		<div class="footer-widget">
 			<div class="prl-container">
@@ -28,16 +36,16 @@
 				<div class="right"><?php echo trim($prl_data['footer_text']);?></div>
 			</div>
 		</div><!-- .copyright -->
-		
+
 	</footer><!-- #footer -->
-	
+
 </div><!-- .site-wrapper -->
-	<?php if($prl_data['switcher']=='Enable') include ('_switcher/index.php');?>  
+	<?php if($prl_data['switcher']=='Enable') include ('_switcher/index.php');?>
 	<a id="toTop" href="#"><i class="fa fa-long-arrow-up"></i></a>
-	
+
 	<?php /* 29Aug16 zig try moving to after body tag -<script data-cfasync="false" type="text/javascript" src="http://ellsworthamerican.me.pw.newsmemory.com/include.php"></script> */ ?>
 	<?php /* technavia footer script */ ?>
-    
+
 	<?php wp_footer();?>
 	<?php echo stripslashes($prl_data['google_analytics']); ?>
 	<?php /* <script defer=defer type="text/javascript">broadstreet.zone(39775);</script> */ ?>
