196171ad45413efaea2f3a1920cd33c3a033ca30
linda@reachmaine.com <linda@reachmaine.com>
2018-10-27 15:00:06 -0400

    more  minimal mode for recipes
    for archives & obits & pages.

---
+++ origmag-ea/Overrides/mu plugins/wpurp-minimal-mode.php
@@ -0,0 +1,14 @@
+<?php
+/* plugin to turn off ultimate recipe plugin on from loading on some pages, for performance sake */
+
+function wpurp_minimal_mode( $minimal_mode, $url ) {
+    if( $url == '/' ) return true; // Homepage
+
+    preg_match( "/^\/user/i", $url, $matches );
+    if( count( $matches ) > 0 ) return true; // Any URL starting with /user
+
+    // dont show for archives(usually category) or single pages or obits 
+    if ( is_post_type_archive(array('page','obituary', 'post')) || is_singular(array('obituary', 'page')) ) return true;
+    return false;
+}
+add_filter( 'wpurp_minimal_mode', 'wpurp_minimal_mode', 10, 2 );
