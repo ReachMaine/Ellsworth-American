c0dbe7c24834b97a72c59f98a4dd8a6978ee83d1
linda@reachmaine.com <linda@reachmaine.com>
2019-09-03 18:46:45 -0400

    optimizing
    only need author box on single posts & add 'no_found_rows' => TRUE  on both plugins.

--- origmag-ea/Overrides/inc/widgets/recent-posts.php
+++ origmag-ea/Overrides/inc/widgets/recent-posts.php
@@ -4,7 +4,7 @@ class recents_posts_Widget extends WP_Widget {
         $widget_ops = array('classname' => 'widget-recent-post', 'description' => __( 'Display latest news with settings', 'presslayer' ) );
         parent::__construct('recent_posts', __( '&raquo; Recent Posts', 'presslayer' ), $widget_ops);
     }
-	
+
 	function widget($args, $instance)
 	{
 		global $theme;
@@ -28,7 +28,7 @@ class recents_posts_Widget extends WP_Widget {
 		$show_reviews = isset($instance['show_reviews']) ? $instance['show_reviews'] : '';
 		$show_excerpt = isset($instance['show_excerpt']) ? $instance['show_excerpt'] : '';
 		$num_excerpt = isset($instance['num_excerpt']) ? $instance['num_excerpt'] : '';
-		
+
 		echo $before_widget;
 		?>
 		<!-- BEGIN WIDGET -->
@@ -44,7 +44,7 @@ class recents_posts_Widget extends WP_Widget {
 		}else{
 			if($title) echo $before_title.$title.$after_title;
 		}
-		
+
 		// Post format
 		$tax_query=array();
         if(!empty($post_format)){
@@ -57,7 +57,7 @@ class recents_posts_Widget extends WP_Widget {
 						)
 					  );
 		}
-		
+
 		//Reviews
 		$rev_query = array();
         if($show_reviews == true){
@@ -68,23 +68,24 @@ class recents_posts_Widget extends WP_Widget {
 					'compare' => '='
 				));
 		}
-		
+
 		$args = array(
 			'post_type' => 'post',
 			'showposts' => $post_count,
 			'cat' => $category,
 			'tax_query' => $tax_query,
 			'meta_query' => $rev_query,
-			'order'=> $order
+			'order'=> $order,
+			'no_found_rows' => TRUE
 		);
-		
+
 		if($sort == 'popular'){
 			$popular_arr = array(
 				'orderby'=>'comment_count',
 				'order'=> $order
 			);
 			$args = array_merge ($args, $popular_arr);
-			
+
 		}else if($sort == 'view'){
 			$view_arr = array(
 				'orderby'   =>  "meta_value_num",
@@ -93,11 +94,11 @@ class recents_posts_Widget extends WP_Widget {
 			);
 			$args = array_merge ($args, $view_arr);
 		}
-		
+
 
 		$recents = new WP_Query($args);
 		?>
-		
+
 		<ul class="recent-list">
 			<?php
 			$i = 0;
@@ -106,11 +107,11 @@ class recents_posts_Widget extends WP_Widget {
 			?>
 			<li class="recent-post-<?php echo $thumbnail;?>">
 				<article class="clearfix">
-						
-						<?php 
+
+						<?php
 						$image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size');
 						$new_image = aq_resize( $image[0], 60, 60, TRUE, FALSE );
-						
+
 						switch($thumbnail){
 							case 'large':
 								if($thumbnail_first == true){
@@ -125,45 +126,45 @@ class recents_posts_Widget extends WP_Widget {
 							?>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" class="prl-thumbnail"><img src="<?php echo $new_image[0];?>" width="60" height="60" alt="<?php the_title_attribute(); ?>"></a>
 							<?php
-							}} else{ 
+							}} else{
 							if(has_post_thumbnail()):
 							?>
-							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" class="prl-thumbnail"><img src="<?php echo $new_image[0];?>" width="60" height="60" alt="<?php the_title_attribute(); ?>"></a>	
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" class="prl-thumbnail"><img src="<?php echo $new_image[0];?>" width="60" height="60" alt="<?php the_title_attribute(); ?>"></a>
 							<?php
 							endif;
 							}
 							break;
-							
+
 							default:
-							break;	
+							break;
 						}
 						?>
-		
-					<?php if($thumbnail == 'large' && $thumbnail_first == true && $i == 1) {?>	
+
+					<?php if($thumbnail == 'large' && $thumbnail_first == true && $i == 1) {?>
 					<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title();?></a> <?php echo get_label_format(get_the_ID());?> <?php echo get_label_meta(get_the_ID());?></h3>
 					<?php }else{?>
 					<h4><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title();?></a> <?php echo get_label_format(get_the_ID());?> <?php echo get_label_meta(get_the_ID());?></h4>
 					<?php }
 					?>
 					<?php if($meta == true) {?><?php post_meta($date,$author,$comment,$cat,$view);?><?php } ?>
-					<?php 
+					<?php
 					if($show_reviews == true) {
-					echo get_rating_star(get_the_ID());	
+					echo get_rating_star(get_the_ID());
 					 }?>
 					<?php if($show_excerpt == true) {?><p><?php echo text_trim(get_the_excerpt(),$num_excerpt,'...');?></p><?php } ?>
 				</article>
 			</li>
 			<?php endwhile; wp_reset_query(); ?>
 		</ul>
-		
+
 		<?php
 		echo $after_widget;
 	}
-	
+
 	function update($new_instance, $old_instance)
 	{
 		$instance = $old_instance;
-		
+
 		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['category'] = (int) $new_instance['category'];
 		$instance['sort'] = $new_instance['sort'];
@@ -212,7 +213,7 @@ class recents_posts_Widget extends WP_Widget {
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category', 'presslayer'); ?>:</label> 
+			<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category', 'presslayer'); ?>:</label>
 			<select id="<?php echo $this->get_field_id('category'); ?>" name="<?php echo $this->get_field_name('category'); ?>" class="widefat" >
 				<option value='all' <?php if ('all' == $category) echo 'selected="selected"'; ?>><?php _e('All categories', 'presslayer'); ?></option>
 				<?php $prl_cats = get_categories('hide_empty=1&depth=1&type=post'); ?>
@@ -221,21 +222,21 @@ class recents_posts_Widget extends WP_Widget {
 				<?php } ?>
 			</select>
 		</p>
-		
-		
+
+
 		<p><label for="<?php echo $this->get_field_id('sort'); ?>"><?php _e('Sort by', 'presslayer'); ?>:</label><br>
            <select class="widefat" name="<?php echo $this->get_field_name('sort') ?>" id="<?php echo $this->get_field_id('sort')  ?>">
                 <option <?php selected($sort, 'latest') ?> value="latest">Latest (time of posts)</option>
                 <option <?php selected($sort, 'popular') ?> value="popular">Popular (number of comments)</option>
                 <option <?php selected($sort, 'view') ?> value="view">View (number of views)</option>
-            </select>   
+            </select>
         </p>
-		
+
 		<p><label for="<?php echo $this->get_field_id('order'); ?>"><?php _e('Order by', 'presslayer'); ?>:</label><br>
            <select class="widefat" name="<?php echo $this->get_field_name('order') ?>" id="<?php echo $this->get_field_id('order')  ?>">
                 <option <?php selected($order, 'DESC') ?> value="DESC">Descending</option>
                 <option <?php selected($order, 'ASC') ?> value="ASC">Ascending</option>
-            </select>   
+            </select>
         </p>
 
 		<p>
@@ -246,16 +247,16 @@ class recents_posts_Widget extends WP_Widget {
 					<option <?php selected($post_format, 'video') ?> value="video">Video</option>
 					<option <?php selected($post_format, 'audio') ?> value="audio">Audio</option>
 					<option <?php selected($post_format, 'gallery') ?> value="gallery">Gallery</option>
-                </select>   
+                </select>
             </label>
         </p>
-		
-		
+
+
 		<p>
 			<label for="<?php echo $this->get_field_id('post_count'); ?>"><?php _e('Number of posts to show', 'presslayer'); ?>:</label>
 			<input id="<?php echo $this->get_field_id('post_count'); ?>" name="<?php echo $this->get_field_name('post_count'); ?>" value="<?php echo $post_count; ?>" type="text" size="3" />
 		</p>
-		
+
 		<p>
             <label for="<?php echo $this->get_field_id('thumbnail') ?>">
                 <?php  _e('Thumbnail','presslayer') ?>
@@ -263,18 +264,18 @@ class recents_posts_Widget extends WP_Widget {
                     <option <?php selected('large', $thumbnail) ?> value="large">Large</option>
                     <option <?php selected('small', $thumbnail) ?> value="small">Small</option>
                  	<option <?php selected('nothumb', $thumbnail) ?> value="nothumb">No Thumbnails</option>
-                </select>   
+                </select>
             </label>
         </p>
-		
+
 		<p>
             <label for="<?php echo $this->get_field_id('thumbnail_first'); ?>" ><input type="checkbox" name="<?php echo $this->get_field_name('thumbnail_first') ?>" id="<?php echo $this->get_field_id('thumbnail_first'); ?>" <?php checked(true, $thumbnail_first); ?>> <?php  _e('Show thumbnail for first post only','presslayer') ?></label>
         </p>
-		
+
 		<div class="meta-info">
             <p>
 				<label for="<?php echo $this->get_field_id('meta'); ?>"><input class="recent-post-meta-info" type="checkbox" <?php checked(true, $meta); ?> id="<?php echo $this->get_field_id('meta'); ?>" name="<?php echo $this->get_field_name('meta'); ?>" /> <?php  _e('Show the meta infomation of post','presslayer') ?></label>
-				
+
 			</p>
             <p>&nbsp;&nbsp; <label for="<?php echo $this->get_field_id('date'); ?>" ><input type="checkbox" <?php disabled( false,  $meta ); ?> name="<?php echo $this->get_field_name('date') ?>" id="<?php echo $this->get_field_id('date'); ?>" <?php checked(true, $date); ?> class="submeta-info" > <?php  _e('Show the date of post','presslayer') ?> </label>
             </p>
@@ -282,28 +283,28 @@ class recents_posts_Widget extends WP_Widget {
             </p>
             <p>&nbsp;&nbsp; <label for="<?php echo $this->get_field_id('comment'); ?>" ><input type="checkbox" <?php disabled( false,  $meta ); ?> name="<?php echo $this->get_field_name('comment') ?>" id="<?php echo $this->get_field_id('comment'); ?>" <?php checked(true, $comment); ?> class="submeta-info" > <?php  _e('Show the comment','presslayer') ?> </label>
             </p>
-			
+
 			<p>&nbsp;&nbsp; <label for="<?php echo $this->get_field_id('view'); ?>" ><input type="checkbox" <?php disabled( false,  $meta ); ?> name="<?php echo $this->get_field_name('view') ?>" id="<?php echo $this->get_field_id('view'); ?>" <?php checked(true, $view); ?> class="submeta-info" > <?php  _e('Show views','presslayer') ?> </label>
             </p>
-			
+
 			<p>&nbsp;&nbsp; <label for="<?php echo $this->get_field_id('cat'); ?>" ><input type="checkbox" <?php disabled( false,  $meta ); ?> name="<?php echo $this->get_field_name('cat') ?>" id="<?php echo $this->get_field_id('cat'); ?>" <?php checked(true, $cat); ?> class="submeta-info" > <?php  _e('Show the category info','presslayer') ?> </label>
             </p>
         </div>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('show_reviews'); ?>"><input class="checkbox" type="checkbox" <?php checked(true, $show_reviews); ?> id="<?php echo $this->get_field_id('show_reviews'); ?>" name="<?php echo $this->get_field_name('show_reviews'); ?>" /> <?php  _e('Show reviews','presslayer') ?></label>
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('show_excerpt'); ?>"><input class="checkbox" type="checkbox" <?php checked(true, $show_excerpt); ?> id="<?php echo $this->get_field_id('show_excerpt'); ?>" name="<?php echo $this->get_field_name('show_excerpt'); ?>" /> <?php  _e('Show excerpt','presslayer') ?></label>
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('num_excerpt'); ?>"><?php _e('Length of Excerpt', 'presslayer'); ?>:</label>
 			<input id="<?php echo $this->get_field_id('num_excerpt'); ?>" name="<?php echo $this->get_field_name('num_excerpt'); ?>" value="<?php echo $num_excerpt; ?>" type="text" size="3" />
 		</p>
-		
-		
+
+
 	<?php }
 }
 
