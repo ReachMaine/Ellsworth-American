a2695d27151c443400de00f10e0497696de3bac1
Linda "zig" Speight <linda@reachmaine.com>
2020-12-04 14:52:08 -0500

    make same as EA's

--- origmag-ea/inc/eai_technav.php
+++ origmag-ea/inc/eai_technav.php
@@ -1,12 +1,12 @@
-<?php 
-/* functions to support tecnavia paywall. 
-	31Jan16 zig 
+<?php
+/* functions to support tecnavia paywall.
+	31Jan16 zig
 */
 
 
 	function eai_technav_loginmenu(){
 
-		$out_string = "";		
+		$out_string = "";
 		$login_url = "#";
 		$out_string .= '<ul class="ea-technav-login ea-leaky-login">';
 		$out_string .=    '<li>';
